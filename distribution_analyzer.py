c4ed1854ee37c8ecef7d1b056a4cb8fcfc40d087
mikeyd477 <51045549+mikeyd477@users.noreply.github.com>
2022-03-27 23:12:02 +0300

    Add files via upload
    Added argument parser.

--- distribution_analyzer.py
+++ distribution_analyzer.py
@@ -11,6 +11,8 @@ import pandas as pd
 import time
 import numpy as np
 import xlsxwriter
+import argparse
+import os
 ################################################################################
 #Start Time
 start_time = time.time()
@@ -39,57 +41,76 @@ def analyze_distribution(df,spike_indicator):
 ################################################################################
 #Function to Write Results to EXCEL XLS FILE
 #
-def print_results_xsl(df):
-	workbook = xlsxwriter.Workbook('Distribution_Analysis.xlsx')
-	worksheet_spike_positive = workbook.add_worksheet('Spike+')
-	worksheet_spike_negative = workbook.add_worksheet('Spike-')
+def print_results_xsl(df,output):
+	workbook = xlsxwriter.Workbook(output)
+	worksheet = workbook.add_worksheet('Statistical_Summary')
 	header_row = 1
 	header_column = 0
 	#Global Headers
+	worksheet.write(0,2,'Spike-')
+	worksheet.write(0,8,'Spike+')
 	#spike negative
 	for attribute_index in range(0,14):
 		###############################################################################
 		#Headers Rows
-		worksheet_spike_negative.write(header_row,header_column,df.columns.values[attribute_index])
-		worksheet_spike_negative.write(header_row+1,header_column,'Min')
-		worksheet_spike_negative.write(header_row+2,header_column,'Max')
-		worksheet_spike_negative.write(header_row+3,header_column,'Median')
-		worksheet_spike_negative.write(header_row+4,header_column,'STD')
+		worksheet.write(header_row,header_column,df.columns.values[attribute_index])
+		worksheet.write(header_row+1,header_column,'Min')
+		worksheet.write(header_row+2,header_column,'Max')
+		worksheet.write(header_row+3,header_column,'Median')
+		worksheet.write(header_row+4,header_column,'STD')
 		#Headers Columns
-		worksheet_spike_negative.write(header_row,header_column+1,'TP1')
-		worksheet_spike_negative.write(header_row,header_column+2,'TP2')
-		worksheet_spike_negative.write(header_row,header_column+3,'TP4')
+		worksheet.write(header_row,header_column+1,'TP1')
+		worksheet.write(header_row,header_column+2,'TP2')
+		worksheet.write(header_row,header_column+3,'TP4')
 		###############################################################################
 		for meas_index in range(0,4):
 			for tp_index in range(0,3):
-				worksheet_spike_negative.write(header_row+meas_index+1,header_column+tp_index+1,results_container[0,meas_index,tp_index,attribute_index])
+				worksheet.write(header_row+meas_index+1,header_column+tp_index+1,results_container[0,meas_index,tp_index,attribute_index])
 		#Update the header row for next attribute
 		header_row += 6
 	#spike positive
-	header_row = 0
-	header_column = 0
+	header_row = 1
+	header_column = 6
 	for attribute_index in range(0,14):
 		###############################################################################
 		#Headers Rows
-		worksheet_spike_positive.write(header_row,header_column,df.columns.values[attribute_index])
-		worksheet_spike_positive.write(header_row+1,header_column,'Min')
-		worksheet_spike_positive.write(header_row+2,header_column,'Max')
-		worksheet_spike_positive.write(header_row+3,header_column,'Median')
-		worksheet_spike_positive.write(header_row+4,header_column,'STD')
+		worksheet.write(header_row,header_column,df.columns.values[attribute_index])
+		worksheet.write(header_row+1,header_column,'Min')
+		worksheet.write(header_row+2,header_column,'Max')
+		worksheet.write(header_row+3,header_column,'Median')
+		worksheet.write(header_row+4,header_column,'STD')
 		#Headers Columns
-		worksheet_spike_positive.write(header_row,header_column+1,'TP1')
-		worksheet_spike_positive.write(header_row,header_column+2,'TP2')
-		worksheet_spike_positive.write(header_row,header_column+3,'TP4')
+		worksheet.write(header_row,header_column+1,'TP1')
+		worksheet.write(header_row,header_column+2,'TP2')
+		worksheet.write(header_row,header_column+3,'TP4')
 		###############################################################################
 		for meas_index in range(0,4):
 			for tp_index in range(0,3):
-				worksheet_spike_positive.write(header_row+meas_index+1,header_column+tp_index+1,results_container[1,meas_index,tp_index,attribute_index])
+				worksheet.write(header_row+meas_index+1,header_column+tp_index+1,results_container[1,meas_index,tp_index,attribute_index])
 		#Update the header row for next attribute
 		header_row += 6
 	workbook.close()
 ################################################################################
+#Parse Command Line Argument Section
+#Create object for parsing command-line options
+parser = argparse.ArgumentParser(description="Command line for Distribution Analyzer")
+#Add arguments to command line parser
+parser.add_argument("-d", "--data", type=str, help="Path to data of distribution output from tree parser")
+parser.add_argument("-o", "--output", type=str, help="Output File Name")
+#Parse the command line arguments to an object
+args = parser.parse_args()
+#Safety if no parameter have been given
+if not args.data:
+	print("No Data File has been given. Please insert DATA file in command line --data")
+	print("For help type --help")
+	exit()
+if not args.output:
+	print("No Output File Name has been given. Please insert Output File Name in command line --output")
+	print("For help type --help")
+	exit()
+################################################################################
 #Read Tree Analyzer Merged Results
-tree_analyzer_df = pd.read_csv("subject022_test.csv")
+tree_analyzer_df = pd.read_csv(args.data)
 #Divide to Spike+ and Spike- Results
 spike_positive_df = tree_analyzer_df.loc[tree_analyzer_df['spike'] == '+']
 spike_negative_df = tree_analyzer_df.loc[tree_analyzer_df['spike'] == '-']
@@ -97,5 +118,5 @@ analyze_distribution(spike_negative_df,0)
 analyze_distribution(spike_positive_df,1)
 np.set_printoptions(suppress=True)
 #print(results_container)
-print_results_xsl(tree_analyzer_df)
+print_results_xsl(tree_analyzer_df,args.output)
 
